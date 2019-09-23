e6bf73e4468ffac58eee3b7180ddc89d6ee0b5a9
Colin McParland <colin@tinybird.ca>
2019-09-22 20:37:00 -0700

    Encrypt deploy key

---
+++ src/common/header/header.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+/* 
+
+    Start Styled Components
+
+*/
+export const HeaderContainer = styled.div`
+    display: grid;
+`;
+
+export const Header: FC = props => ();
